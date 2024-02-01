fa753b6e9f791370872f812913930cc476386a0d
Carson <p1833247938@163.com>
2024-02-01 15:28:37 +0800

    更新

---
+++ Manual_Testing/ResponseLog.js
@@ -0,0 +1,98 @@
+[2024-02-01 15:23:18]
+{
+    "StatusCode": 200,
+    "ResponseTime": 593,
+    "ResponseBody": {
+        "RequestID": "u7hF3PK2FwxtlgXa",
+        "RequestType": "0005",
+        "ResponseCode": "1",
+        "ErrorCode": "0000",
+        "ErrorMessage": "成功",
+        "Data": {
+            "ApplyPolicyRef": "I8352717226064699425",
+            "PolicyRef": "H240201000468710182919",
+            "AgencyPolicyRef": "1jdi0Jlldy6GCwqB",
+            "PlanCode": "TK2024011904",
+            "EffectiveDate": "20240202000000",
+            "ExpireDate": "20240302235959",
+            "Currency": "CNY",
+            "TotalPremium": "0",
+            "PolicyUrl": "http://www-tk-cn-test-wh-tkc.tk.cn/sp-greedy/sit?api_s=document.property&api_m=policy.download&applicantName=30D14BF87C121A44BF67D107C56DE89D96CF120D115D107A75D124B99&policyNo=E106CE56A94AF65D21DE106B21D110DE37BF36D103B79CE102C125CF47A83AE119A94BF2AF15BE64BF92DF127A51CE63B36DE17AE103C11CF54A105AF90",
+            "PremiumPlanList": [
+                {
+                    "PolicyRef": "H240201000468710182919",
+                    "InstallmentNo": 1,
+                    "InstallmentPremium": "0",
+                    "EffectiveDate": "20240202000000",
+                    "ExpireDate": "20240302235959",
+                    "Currency": "CNY"
+                }
+            ]
+        }
+    }
+}
+[2024-02-01 15:24:29]
+{
+    "StatusCode": 200,
+    "ResponseTime": 897,
+    "ResponseBody": {
+        "requestId": "8283940780878766087",
+        "responseTime": "20240201152429",
+        "responseCode": "000_000_000",
+        "responseMsg": "??"
+    }
+}
+[2024-02-01 15:25:06]
+{
+    "StatusCode": 200,
+    "ResponseTime": 219,
+    "ResponseBody": {
+        "requestId": "8283940780878766087",
+        "responseTime": "20240201152506",
+        "responseCode": "000_000_000",
+        "responseMsg": "??"
+    }
+}
+[2024-02-01 15:25:32]
+{
+    "StatusCode": 200,
+    "ResponseTime": 464,
+    "ResponseBody": {
+        "code": "1"
+    }
+}
+[2024-02-01 15:25:46]
+{
+    "StatusCode": 200,
+    "ResponseTime": 253,
+    "ResponseBody": {
+        "code": "1"
+    }
+}
+[2024-02-01 15:27:18]
+{
+    "StatusCode": 200,
+    "ResponseTime": 148,
+    "ResponseBody": {
+        "requestId": "5c85524dc4fa467cb5215567fe26a7a7",
+        "responseTime": "20240201152718",
+        "responseCode": "000_000_000",
+        "responseMsg": "??"
+    }
+}
+[2024-02-01 15:27:34]
+{
+    "StatusCode": 200,
+    "ResponseTime": 195,
+    "ResponseBody": {
+        "code": "1"
+    }
+}
+[2024-02-01 15:27:45]
+{
+    "StatusCode": 200,
+    "ResponseTime": 184,
+    "ResponseBody": {
+        "code": "1"
+    }
+}
--- Manual_Testing/回调/泰康团险出单回调.py
+++ Manual_Testing/回调/泰康团险出单回调.py
@@ -15,38 +15,102 @@ class Order_callback:
         self.host = config.get_value(self.environment, "callback_host")
 
     def Order_callback(self):
-        url = "/issuingmc/channelopenapi/directPay/issue/callback"
+        url = "/issuingmc/channelopenapi/tk/group/insure/callback"
         request_url = self.host + url
-        body = (
-            "t6esmS3+RKtFqlQNHLzY5Zm4ZtfOxcP5te0XQYues8YbP5sY4GLrG8gsGpSDZ9wGb7hdvwujKA0IqSm6frIamGZFg7eKyq5h9ro/"
-            "F/tZdo6S14K1qWj7BjUWC8gRCRR0cMNu+RaA7hxnYCYjsQPHNaWzCCEEV4t6fLckaof97M2sh1dFBgFXJxTVkaGBGwyMdgQRBfw8"
-            "Ba/O7jIpCvSlZW+8Qkg3UwuzCT97oNLB2hLW/lzdPLiXFdIFGw/3fWPMvOcn+KStdVoTel5zYZQbKYvAUUf+s132oTUFIA967DVi"
-            "Pyxa1uU91vl7h2BN1Kb7CkgRdQhham9v1xr+vMP+K8uzC6sHvYb6ws5Gv8a3DIjdRM1/PUrNTDIwVGUPhc+9vRsOOV1caFcyOFq+B"
-            "6xcMtKpdSuX/FTsON9TUnO6Y7OUMd2fis8nm2qKEEcg5IrL2EtcGXitur5+3iieed0mkdaZHPbDzTH+S+CSy0sbHnNJG1XzAr/NVS"
-            "dbvVlOEmrFKYoL6USk7sap8OxjtZ4DxnLQj8GHDcEAilf85dVwOIPBTyVozD5+A0TFLuCzO8qC7RAcumx3JlmFtIus6o+kkoVqWAb0"
-            "Tw8WtOJTyLq1tDvo7azrhAKGIGx1tHOM2jVj34AOZcKn88PxBA+NKTVAd/KgV8MyiH6oIfVW2eD90LpLLX2io+lWDFd82dt4DLa9KiU"
-            "902Chxh/g8fdBxInUsrD5aPIlVAz2R1uGD6pgtKNxxmxVHz6WVrPdAEqRkw9hBddvHY+dGArPMAVWrGXsM+yNdJoueoQ0L4sdeKnHcf"
-            "12w6m72DPqLloVyWRARFaDT74tbGOSt58HIRlm31E70Ix8TuYAMn18aep9IxlsBMPDKmACDuzZKB6UTZE6V2IkKjaIffjvsbh07RxLL"
-            "/PSdL84GBs1UxSKGLF6feduRKprZYfUhBmjQLuFV+uw7cloiLcCLzhLDPfFh7njCx/ingzbSu7RIlRZB2Fw87jJleR+woGZYw4eawQK"
-            "vawTqQnUh5h+8Ww0Us0Nf3bLJ1LSbl3oj225UZbG2Ectl8pGdlS7qo2lZscRJDd54ehP2uPS04VWb2X1xbNqAp/fUMS3by0eq37dL0rx"
-            "tKMC54/FEu48q/yEIQxngXV/oBkqRDofwgy1pOGSljT7RbMNer4HKB5eTH1QQnYtqJkD/6KeI+MAVYx/jNjwJFD6xqpj4mxL6yg7DNET8"
-            "BEYFNU4CCDrqWYosgVUp/QGfCoccJLGk3iEGgu3EMiZfEd8Mkk7hCj4roj7Db6/8w4HefS2l2opee8tIK73qIYmXbQXUA3gV7KnDny8eJ"
-            "WEmXm3CRkJsUCCTbp4F2Ts092LzxUZkDyoGjpFcqxX+TWXTPSqqu9uiz/dwIzROPcfWE2XF5g1gyKXJL8OB+0ThD/GsHTjLWs8lLpb7Wc"
-            "dekaDLIkclCvk/3DmXTEQ06x3DBVX4WYlRD1ZNOv9Nw0S3gV2TW/BdclLXH9fl/pA5MOnVoPwIFu8EhdNnfYN5Plra2VJQBkh0vmj3Uvn"
-            "7BE1jdp0/R+yQsRUi6ilsHWhXcba6RqRbsk4JB4yMnAc5R1HMYEqafFzbIhKOfS9ccWEvtlmheW5+C+p6c1ySYszMzDfsdOzcEzvqcjFn"
-            "OGJuWh0jBE32UNfRwC40FMkR3fT75LuzHcehw83/RNOqSjfZ5Xa5TnJ1hXYWWUHk4nWpSQQt6++kxGF81I1mXZiHOJbuMLbeUlil6tuXi"
-            "6qBesqDgR2B7Suz2AlTYn0I5BXz16of+uItpTMNNMLSvpGh37z+pIH2xOsoAyhqkURTd26QuD+xDK2q8MYVYxVvwXbSAkJ0aKG7jHMbyk"
-            "U1LXWUpA6/BrJU5dCpxIRq+FioH02mLgpH2XWicItdCWr3jm0SRsrC53p/JepFuplvzgYGzVTFIoYsXp9525EqgOOeyYFWYu5YObjKDe0"
-            "fNyItwIvOEsM98WHueMLH+KeDVqpfvMhq3BDRmDWI3eEkn7CgZljDh5rBAq9rBOpCdSHmH7xbDRSzQ1/dssnUtJuXeiPbblRlsbYRy2Xy"
-            "kZ2VL/6JCczb680Huy7Mt/dRZm8NwsWQw54iRenDtdOFFQ0PG1Cz/W2gGkX9BsT5uyExRecOXEhg+KCsx4ZCb1TPnTDkUb3KeTruNMOFO"
-            "0Gb3jxw/5jAOzH/UiMeMDYCCySQ357OIYtArCSZ4TfOZJLoTlcHtEoCr1HmGrN1q3FBkwLjXxvkZv3e/YJRprcIdzWiiraApGcqHF1kip"
-            "EI6YmOoEXw3BRTIhNTZz/Bb7zJCRQ3yM11eIS8JV4oH0aeKegfwBJaQ32Ue4vg9Z4+4Xh980C+VrzkAQ8CEyhZUfv6kefo0PDCkJsaZjX"
-            "rn0ZEfLMTy8P7UpLgvtVAa0kgxqgyIwETl4hiKIlsSSdDo4iUES6lrHcJHnyC+W7ZC6czIaZXnR4cMlDj501JAlbYsM5IPWJGa0S4RFs4"
-            "b+0l4wZTrseDHihotRvqtOTB4kwcmpfvwWgJd2/nWpH+K6yrEIoCbEih1aVoMWt3K8MujJj9PrImjuj/lHU6jOf7JJiAnyJmjYDS6bUC7"
-            "z2/R3x9hWNmaCMfE7mADJ9fGnqfSMZbATDFy9ECa5ojEpkR/0EEM00d5rng0GliuhNK25+6IWLMC/ItLfQUdJow66oSb6cVabUIpVMYiZ"
-            "QFwEkDrnjgC6dfy7HXc6BxGi7tQCTEWEXtKdSOljjzP46DiXZvj0Zpuj+5rpbRfNumlEakWMaFcBd0GS37ZkujFekn0CZIPo+HzgKH41R"
-            "njaR/hFwc9yVEZWjNuQB9OJ9y99IveGtGL/zQZmUykh3D7gBG9ahjXscE9gtzhsU4HMBV7yWX/e2eP5gnZ2fNo6cfTRBmPlGqVwsePF06"
-            "58oDiXOWuJVtaxEyt/MvChw8puqDizJTZSDeo5+zaoG9dEdNPoJ08KXE2jN+Q==")
+        body = {
+            "requestTime": "20240109110503",
+            "requestId": "8283940780878766087",
+            "requestData": {
+                "utmSource": "",
+                "groupPolicyNo": "8H2405DA1A091S8",
+                "productCode": "10N01500",
+                "groupEpolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41DE52B87D28DF71DF96CE76DE6AE98BE23AF107A17CE5AE60B92DE87&policyNo=92C85A38A125DF124CE60BE23CE117C113B87D60C1DF120DE34B81C31",
+                "personalPolicyList": [{
+                    "personalPolicyNo": "6H2405DA1A3K1T6",
+                    "effectiveTime": "20240110000000",
+                    "openid": "",
+                    "channelPolicyNo": "I8281611053178454097",
+                    "Premium": 190,
+                    "personalTxtJSONString": "{\"nationality\":\"CN\"}",
+                    "Openid": "",
+                    "expiredTime": "20250109235959",
+                    "credentialType": "01",
+                    "credentialNo": "530500199801072947",
+                    "premium": 190,
+                    "ExpiredTime": "20250109235959",
+                    "name": "纳吉丹烟",
+                    "epolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41AF52D87C28CE71BF96CF76AE6AE98CF23BE107B17AF5AF60C92CF87&policyNo=E60A27AF122AF125B119A41A7AF6DE3DF89A53CF115AE55AE90DF85AF27",
+                    "status": 1
+                }, {
+                    "personalPolicyNo": "6H2405DA1A3K1N6",
+                    "effectiveTime": "20240110000000",
+                    "openid": "",
+                    "channelPolicyNo": "I8281611053178454095",
+                    "Premium": 190,
+                    "personalTxtJSONString": "{\"nationality\":\"CN\"}",
+                    "Openid": "",
+                    "expiredTime": "20250109235959",
+                    "credentialType": "01",
+                    "credentialNo": "230400199801077813",
+                    "premium": 190,
+                    "ExpiredTime": "20250109235959",
+                    "name": "钳耳静曼",
+                    "epolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41BE52A87B28BE71CF96BF76DE6DF98BF23BF107A17AE5CF60A92CE87&policyNo=F37B38C45A77BE96A95DE101AF109D7B18C40C101A6DE73D127D119",
+                    "status": 1
+                }, {
+                    "personalPolicyNo": "6H2405DA1A3K1Y6",
+                    "effectiveTime": "20240110000000",
+                    "openid": "",
+                    "channelPolicyNo": "I8283940780878766084",
+                    "Premium": 190,
+                    "personalTxtJSONString": "{\"nationality\":\"CN\"}",
+                    "Openid": "",
+                    "expiredTime": "20250109235959",
+                    "credentialType": "01",
+                    "credentialNo": "360921199801072244",
+                    "premium": 190,
+                    "ExpiredTime": "20250109235959",
+                    "name": "毛又蓝",
+                    "epolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41DF52C87C28CE71BF96BE76DF6CF98AF23AF107C17CE5CE60D92DF87&policyNo=E68DE41CF49A113CE28B104BF107DE91A95A112A79D37A122BF80BF66B73",
+                    "status": 1
+                }, {
+                    "personalPolicyNo": "6H2405DA1A3K206",
+                    "effectiveTime": "20240110000000",
+                    "openid": "",
+                    "channelPolicyNo": "I8283940780878766085",
+                    "Premium": 190,
+                    "personalTxtJSONString": "{\"nationality\":\"CN\"}",
+                    "Openid": "",
+                    "expiredTime": "20250109235959",
+                    "credentialType": "01",
+                    "credentialNo": "542129199801072466",
+                    "premium": 190,
+                    "ExpiredTime": "20250109235959",
+                    "name": "责夜白",
+                    "epolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41BE52C87B28DE71CE96AF76DE6CE98DE23AE107C17AF5DF60D92AE87&policyNo=E54C23B115CE96BF86BE31B69BF109CF83B0DE51BE22C49B98A51D113",
+                    "status": 1
+                }, {
+                    "personalPolicyNo": "6H2405DA1A3K1S6",
+                    "effectiveTime": "20240110000000",
+                    "openid": "",
+                    "channelPolicyNo": "I8281611053178454096",
+                    "Premium": 190,
+                    "personalTxtJSONString": "{\"nationality\":\"CN\"}",
+                    "Openid": "",
+                    "expiredTime": "20250109235959",
+                    "credentialType": "01",
+                    "credentialNo": "659000199801070851",
+                    "premium": 190,
+                    "ExpiredTime": "20250109235959",
+                    "name": "长元从梦",
+                    "epolicyUrl": "http://ecuat.taikang.com/sp-greedy/api/v2/?api_s=document.property&api_m=policy.download&applicantName=41CF52D87D28AE71CE96AF76AE6CF98BF23BE107B17BF5AE60C92CF87&policyNo=8DE46D81A37C85C127AF44CF1CE124C61BE77DF124BE10DE26CF123DF19",
+                    "status": 1
+                }],
+                "requestFlag": "1",
+                "channelContractNo": "G8283940609083121695",
+                "channelCode": "086000001042"
+            }
+        }
 
         return SendMethod.post_json(url=request_url, json=body)
 
