67e6655f83b823a99fbbc115918222adb0825a03
Carson <p1833247938@163.com>
2024-02-01 17:10:39 +0800

    更新

---
+++ Manual_Testing/ResponseLog.js
@@ -0,0 +1,72 @@
+[2024-02-01 16:53:21]
+{
+    "StatusCode": 200,
+    "ResponseTime": 294,
+    "ResponseBody": {
+        "RequestID": "0s5uRE2suVD4zhkb",
+        "RequestType": "0006",
+        "ResponseCode": "1",
+        "ErrorCode": "0000",
+        "ErrorMessage": "成功",
+        "Data": {
+            "ApplyPolicyRef": "I8352793607759626285",
+            "AgencyPolicyRef": "IpyvPOeWVBSI6BSO",
+            "InsuredPremiumList": [
+                {
+                    "InsuredId": "6",
+                    "InsuredName": "深圳市欢太数字科技有限公司",
+                    "ResultFlag": "1",
+                    "ErrorMsg": "成功"
+                }
+            ]
+        }
+    }
+}
+[2024-02-01 16:59:08]
+{
+    "StatusCode": 200,
+    "ResponseTime": 305,
+    "ResponseBody": {
+        "RequestID": "sNiMKRVanIKfeCZl",
+        "RequestType": "0006",
+        "ResponseCode": "1",
+        "ErrorCode": "0000",
+        "ErrorMessage": "成功",
+        "Data": {
+            "ApplyPolicyRef": "I8352793607759626286",
+            "AgencyPolicyRef": "eQcrGImyUMM0uthq",
+            "InsuredPremiumList": [
+                {
+                    "InsuredId": "6",
+                    "InsuredName": "深圳市欢太数字科技有限公司",
+                    "ResultFlag": "1",
+                    "ErrorMsg": "成功"
+                }
+            ]
+        }
+    }
+}
+[2024-02-01 17:00:15]
+{
+    "StatusCode": 200,
+    "ResponseTime": 283,
+    "ResponseBody": {
+        "RequestID": "PcCDZzHi4rulPXzz",
+        "RequestType": "0006",
+        "ResponseCode": "1",
+        "ErrorCode": "0000",
+        "ErrorMessage": "成功",
+        "Data": {
+            "ApplyPolicyRef": "I8352793607759626287",
+            "AgencyPolicyRef": "ioGkUsHxRdsnaEU9",
+            "InsuredPremiumList": [
+                {
+                    "InsuredId": "6",
+                    "InsuredName": "深圳市欢太数字科技有限公司",
+                    "ResultFlag": "1",
+                    "ErrorMsg": "成功"
+                }
+            ]
+        }
+    }
+}
