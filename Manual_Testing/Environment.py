41f19aa2cfe3e29e6ccdefd2e249ccaadf2219c7
Carson <p1833247938@163.com>
2024-02-22 10:57:46 +0800

    更新

--- Manual_Testing/Environment.py
+++ Manual_Testing/Environment.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""执行环境(全局):prj/sit/stab/pre/prd_b/prd_z/
----/OC_prj/OC_sit/OC_prd/OC_prd2/OC_prd_oppo
----/OC_prd_vivo/OC_prd_vivo2"""
+"""执行环境(全局)
+老系统环境:prj--sit--stab--pre--prd_z--prd_b
+OPPO云环境(pn代表公网):OC_prj--OC_prj_pn--OC_sit--OC_sit_pn--OC_prd--OC_prd_pn--OC_prd2--OC_prd2_pn
+OPPO云转发老系统vivo环境:OC_prd_vivo--OC_prd_vivo2
+"""
 
 Environment = "OC_prj"
 """
