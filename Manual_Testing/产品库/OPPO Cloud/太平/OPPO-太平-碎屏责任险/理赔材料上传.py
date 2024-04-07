bd40b272c7384b2c2cf2a40f4a2ef6e720b4e793
Carson <p1833247938@163.com>
2024-04-07 15:23:44 +0800

    更新

--- Manual_Testing/产品库/OPPO Cloud/太平/OPPO-太平-碎屏责任险/理赔材料上传.py
+++ Manual_Testing/产品库/OPPO Cloud/太平/OPPO-太平-碎屏责任险/理赔材料上传.py
@@ -22,7 +22,7 @@ class Material_upload:
         request_url = self.host + url
         body = {
             "Data": {
-                "PolicyNo": "EP202403640077372178",  # 保单号
+                "PolicyNo": "EP202403640000017272",  # 保单号
                 "MaterialName": "维修前照片.jpg",  # 材料名，示例：xxx.png
                 "MaterialType": "2",  # 材料类型：1 - 证件照 2、维修前照片 3、维修后照片
                 "MaterialFileType": "FILE_URL",  # 材料文件类型：FILE_URL - 文件地址、BASE64 - base64数据流
