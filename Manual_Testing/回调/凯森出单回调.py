a7286229c48ee3bd85313ff0e271158078780157
Carson <p1833247938@163.com>
2024-04-25 17:51:05 +0800

    新增:凯森出单回调

---
+++ Manual_Testing/回调/凯森出单回调.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from Manual_Testing.common.operation_config import Config
+import json, sys
+from Manual_Testing.common.send_method import SendMethod
+from Manual_Testing.common.RandomNumber import RandomStr, Execution_Time
+from Manual_Testing.Environment import Environment
+from Manual_Testing.common.PrintData import Logger
+
+config = Config("config.ini")
+
+
+class Status_callback:
+    def __init__(self):
+        self.environment = Environment
+        self.host = config.get_value(self.environment, "callback_host")
+
+    def Status_callback(self):
+        url = "/issuingmc/channelopenapi/ks20220420/policy/callback"
+        request_url = self.host + url
+        body = {
+            "extId": "B8602218267201454262",  # 业务单号
+            "msg": "成功",  # 返回信息
+            "policyNo": "",  # 保单号
+            "status": "0",  # 状态(成功：0失败：1未知：2)
+            "product": "WDDE10",  # 产品编码
+            "inid": "1"  # 保司编码
+        }
+        return SendMethod.post_jsonx(url=request_url, json=body)
+
+
+"""
+太平任逍遥意外伤害保险（互联网专属）; (product: 1303104237, inid: 15), 保障期限: 3个月（自然月）, 推送渠道"AtcualPlanCode":"KS20240423F0401"
+太平众安心住院医疗保险; (product: 1304104136, inid: 15), 保障期限: 6个月（自然月）,  推送渠道"AtcualPlanCode":"KS20240423F0402"
+招商信诺海陆空意外伤害保险产品; (product: HLKYW, inid: 11, 不传输保单号), 保障期限: 90天,  推送渠道"AtcualPlanCode":"KS20240423F0403"
+阳光融客-出行无忧意外保障2023款; (product: WDDE10, inid: 1, 不传输保单号), 保障期限: 1个月（自然月）,  推送渠道"AtcualPlanCode":"KS20240423F0404"
+"""
+
+if __name__ == "__main__":
+    sys.stdout = Logger()
+    Res = Status_callback().Status_callback()
+    print(f'[{Execution_Time()}]\n{Res}')
