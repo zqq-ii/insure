3e082c92ec606bf5accca77ecdb8a36e1e5702ea
Carson <p1833247938@163.com>
2024-04-30 10:59:40 +0800

    略

--- Manual_Testing/产品库/OPPO Cloud/泰康/OPPO-泰康-特定传染病危重症保障/一步出单_同步.py
+++ Manual_Testing/产品库/OPPO Cloud/泰康/OPPO-泰康-特定传染病危重症保障/一步出单_同步.py
@@ -31,7 +31,7 @@ class One_order:
                     "PlanCode": "TK2022122004",  # 计划代码
                     "IssueDate": Time(),  # 出单时间
                     "EffectiveDate": Tomorrow(),  # 生效时间
-                    "ExpireDate": SeveralDays(),  # 失效时间
+                    "ExpireDate": SeveralDays(30),  # 失效时间
                     "GroupSize": "1",  # 被保人个数
                     "TotalPremium": "0",  # 总保费
                     "Currency": "CNY",  # 币别
