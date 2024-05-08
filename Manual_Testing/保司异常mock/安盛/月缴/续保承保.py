4ed1ce600e0ea0d55458d2dc0df66d785c43e300
Carson <p1833247938@163.com>
2024-05-08 10:49:26 +0800

    更新

--- Manual_Testing/保司异常mock/安盛/月缴/续保承保.py
+++ Manual_Testing/保司异常mock/安盛/月缴/续保承保.py
@@ -2,9 +2,8 @@
 from Manual_Testing.common.operation_config import Config
 import json, sys
 from Manual_Testing.common.send_method import SendMethod
-from Manual_Testing.common.communal import RandomStr, Execution_Time
+from Manual_Testing.common.communal import RandomStr, Execution_Time, Logger
 from Manual_Testing.Environment import Environment
-
 
 config = Config("config.ini")
 
